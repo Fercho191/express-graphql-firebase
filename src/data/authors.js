aa8453b817a9ce1d4b9701fdfbcb10e2cc6ad23d
Fernando Espinoza <fercho.esp1818@gmail.com>
2017-10-29 12:48:54 -0500

    Updating on item removed

--- src/data/authors.js
+++ src/data/authors.js
@@ -14,9 +14,16 @@ ref.on("child_changed", (snapshot) => {
 
 ref.on("child_added", (snapshot) => {
   let item = snapshot.val();
-  item.id = snapshot.key
+  item.id = snapshot.key;
   Authors.push(item) 
 });
 
-
+ref.on("child_removed", (snapshot) => {
+  let item = snapshot.val();
+  item.id = snapshot.key;
+  let index = Authors.findIndex((author) => {
+    return author.id == item.id
+  }, item.id);
+  Authors.splice(index, 1);
+})
 module.exports = Authors; 
