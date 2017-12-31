89df71fade4e2abc5945876c3e28f34da2415a39
Fernando Espinoza <fercho.esp1818@gmail.com>
2017-12-31 08:11:00 -0500

    First steps to graphql mutations

--- src/schema.js
+++ src/schema.js
@@ -10,6 +10,8 @@ let {
   GraphQLString,
   GraphQLList,
   GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLID,
   // This is used to create required fileds and arguments
   GraphQLNonNull,
   // This is the class we need to create the schema
@@ -38,9 +40,52 @@ const QueryRootType = new GraphQLObjectType({
     })
   });
 
+let AuthorInput = new GraphQLInputObjectType({
+  name: "AuthorInput",
+  fields: {
+    firstName : { type: new GraphQLNonNull(GraphQLString) },
+    lastName : { type: new GraphQLNonNull(GraphQLString) },
+    twitterHandle : { type: GraphQLString, defaultValue: "" }
+  }
+})
+
+const MutationRootType = new GraphQLObjectType({
+  name: 'MutationRootType',
+  description: "Application Schema Mutation Root",
+  fields: {
+    createAuthor: {
+      type: AuthorType,
+      args: {
+          firstName : { type: new GraphQLNonNull(GraphQLString) },
+          lastName : { type: new GraphQLNonNull(GraphQLString) },
+          twitterHandle : { type: GraphQLString, defaultValue: "" }
+      },
+      description: "Create a author",
+      resolve: function(source, args, context, info){
+        console.log(args)
+        return args;
+      }
+    },
+    UpdateAuthor: {
+      type: AuthorType,
+      args: {
+          firstName : { type: new GraphQLNonNull(GraphQLString) },
+          lastName : { type: new GraphQLNonNull(GraphQLString) },
+          twitterHandle : { type: GraphQLString, defaultValue: "" },
+          id: { type: GraphQLID}
+      },
+      description: "Update a author",
+      resolve: function(source, args, context, info){
+        console.log(args)
+        return args;
+      }
+    }
+  }
+});
   // This is the schema declaration
 const AppSchema = new GraphQLSchema({
-    query: QueryRootType
+    query: QueryRootType,
+    mutation: MutationRootType
     // If you need to create or updata a datasource, 
     // you use mutations. Note:
     // mutations will not be explored in this post.
