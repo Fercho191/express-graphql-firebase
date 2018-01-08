6e8b31c211a156b082fa985b6c7a812d5864638f
Fernando Espinoza <fercho.esp1818@gmail.com>
2018-01-08 13:00:51 -0500

    AuthorModel exdends BaseModel

---
+++ src/models/authorModel.js
@@ -0,0 +1,36 @@
+const BaseModel = require('./baseModel');
+
+class AuthorModel extends BaseModel {
+    constructor(){
+        super('authors', 'Author')
+    }
+
+    createAuthor(author) {
+        let newPushRef = this.ref.push({
+          firstName: author.firstName,
+          lastName: author.lastName,
+          twitterHandle: author.twitterHandle
+        })
+        return author
+      }
+      
+    deleteAuthor(id) {
+        this.ref.child(id).set(null, e => {
+          if (e)
+            return false  
+          else 
+            return true
+        })
+        return true
+    }
+      
+    updateAuthor(author, id) {
+        const authorRef = this.ref.child(id)
+        return authorRef.once("value", (snapshot)=> {
+          if (snapshot.val())
+            authorRef.update(author)
+        })
+    }
+}
+
+module.exports = AuthorModel;
